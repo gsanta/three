f72316aaac4f8cda67bc4ff9d1adeaa953192fdc
Gergely Santa <gergely.santa@bitrise.io>
2022-01-03 22:31:07 +0100

    postgres docker configured

--- Gemfile
+++ Gemfile
@@ -26,6 +26,8 @@ gem 'rack-cors', :require => 'rack/cors'
 
 gem 'sqlite3'
 
+gem 'pg'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_01_01_104726) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "sprite_sheets", force: :cascade do |t|
     t.integer "columns"
     t.string "name"
