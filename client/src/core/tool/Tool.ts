02462f0960b20aab7b948e320674ccd0e2cb3e0f
Gergely Santa <gergely.santa@bitrise.io>
2022-05-02 20:31:13 +0200

    Rectangle tool

--- client/src/core/tool/Tool.ts
+++ client/src/core/tool/Tool.ts
@@ -1,12 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import PointerData from './PointerData';
 import ToolType from './ToolType';
 
-interface Tool {
-  name: string;
-  type: ToolType;
-  click(pointer: PointerData): void;
-  move(pointer: PointerData): void;
-  drag(pointer: PointerData): void;
+abstract class Tool {
+  name: string | undefined;
+
+  type: ToolType | undefined;
+
+  click(pointer: PointerData): void {}
+
+  move(pointer: PointerData): void {}
+
+  drag(pointer: PointerData): void {}
 }
 
 export default Tool;
