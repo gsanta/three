3359cd6014f28c1d9f0e341d2e3f727fbe0de44b
Gergely Santa <gergely.santa@bitrise.io>
2022-06-04 01:49:55 +0200

    rubocop-rspec added

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,9 +3,7 @@
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
-# rubocop:disable Layout/LineLength
 abort('The Rails environment is running in production mode!') if Rails.env.production?
-# rubocop:enable Layout/LineLength
 require 'rspec/rails'
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
