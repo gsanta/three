7132dd01e08d4b198a589a7452f1863d55a4cf3d
Gergely Santa <gergely.santa@bitrise.io>
2022-06-10 22:31:21 +0200

    testing-react version fix

--- client/test/setupJest.ts
+++ client/test/setupJest.ts
@@ -4,8 +4,7 @@ import '@testing-library/jest-dom/extend-expect';
 import { setGlobalConfig } from '@storybook/testing-react';
 import * as globalStorybookConfig from '../.storybook/preview';
 
-// TODO investigate why any is needed
-setGlobalConfig(globalStorybookConfig as any);
+setGlobalConfig(globalStorybookConfig);
 
 Object.defineProperty(window, 'matchMedia', {
   writable: true,
