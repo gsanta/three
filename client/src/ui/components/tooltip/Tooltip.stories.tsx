51d6ad94565d8730993f70ad632c3bda524db794
Gergely Santa <gergely.santa@bitrise.io>
2022-09-12 09:36:35 +0200

    Tooltip started

---
+++ client/src/ui/components/tooltip/Tooltip.stories.tsx
@@ -0,0 +1,20 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import React from 'react';
+import Button from '../button/Button';
+import Tooltip from './Tooltip';
+
+export default {
+  component: Tooltip,
+  title: 'Components/Tooltip',
+} as ComponentMeta<typeof Tooltip>;
+
+const Template: ComponentStory<typeof Tooltip> = (props) => (
+  <Tooltip {...props}>
+    <Button>Hover me</Button>
+  </Tooltip>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  label: 'Tooltip text',
+};
---
+++ client/src/ui/components/tooltip/Tooltip.theme.ts
@@ -0,0 +1,17 @@
+import { SystemStyleObject } from '@chakra-ui/react';
+
+const TooltipTheme: SystemStyleObject = {
+  baseStyle: {
+    bg: 'orange.300',
+    color: 'orange.800',
+    fontSize: '12',
+    lineHeight: '2',
+    paddingX: '12',
+    paddingY: '8',
+    borderRadius: '8',
+    // boxShadow: 'tooltip',
+    // zIndex: 'tooltip',
+  },
+};
+
+export default TooltipTheme;
