4774c4a919359c87bc0e587bf631e204715c2906
Gergely Santa <santagergely90@gmail.com>
2022-11-28 01:13:20 +0100

    Reducing pointers

--- engine/Sparky-core/src/app/editor.cpp
+++ engine/Sparky-core/src/app/editor.cpp
@@ -10,15 +10,17 @@ namespace spright_app {
 		m_Services = new spright_app::Services();
 		m_Services->setEmService(new EmService(m_Services->getEventHandler()));
 
-		m_toolHandler = new ToolHandler(m_Window, m_DocumentHandler, editorConfig);
-		m_toolHandler->addTool(new BrushTool(m_DocumentHandler, editorConfig, m_Services, m_Services->getEventHandler()));
-		m_toolHandler->addTool(new RectangleTool(m_DocumentHandler, m_Services, m_Services->getEventHandler()));
-		m_toolHandler->addTool(new EraseTool(m_DocumentHandler, m_Services->getEventHandler()));
-		m_toolHandler->addTool(new PanTool(m_DocumentHandler->getActiveDocument()->getCamera()));
-		m_toolHandler->addTool(new ZoomTool(m_DocumentHandler->getActiveDocument()->getCamera()));
-		m_toolHandler->addActiveTool("zoom");
-		m_toolHandler->addActiveTool("pan");
-		m_toolHandler->addActiveTool("brush");
+		ToolHandler toolHandler(m_Window, m_DocumentHandler, editorConfig);
+		toolHandler.addTool(new BrushTool(m_DocumentHandler, editorConfig, m_Services, m_Services->getEventHandler()));
+		toolHandler.addTool(new RectangleTool(m_DocumentHandler, m_Services, m_Services->getEventHandler()));
+		toolHandler.addTool(new EraseTool(m_DocumentHandler, m_Services->getEventHandler()));
+		toolHandler.addTool(new PanTool(m_DocumentHandler->getActiveDocument()->getCamera()));
+		toolHandler.addTool(new ZoomTool(m_DocumentHandler->getActiveDocument()->getCamera()));
+		toolHandler.addActiveTool("zoom");
+		toolHandler.addActiveTool("pan");
+		toolHandler.addActiveTool("brush");
+
+		m_toolHandler = toolHandler;
 
 		m_Window->getFrameHandler()->registerListener(this);
 	}
@@ -26,7 +28,6 @@ namespace spright_app {
 	Editor::~Editor()
 	{
 		m_Window->getFrameHandler()->unRegisterListener(this);
-		delete m_toolHandler;
 		delete m_Window;
 		delete m_DocumentHandler;
 		delete m_Services;
