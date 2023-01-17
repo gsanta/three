9adc495f3beb1a9903eb6a3c337a90146b34c35d
Gergely Santa <santagergely90@gmail.com>
2023-01-17 19:34:23 +0100

    push commit to github

--- frontend-server/src/index.js
+++ frontend-server/src/index.js
@@ -4,5 +4,5 @@ var { resolve } = require('path');
 
 
 connect()
-  .use(serveStatic(resolve(__dirname, '../dist/website')))
+  .use(serveStatic(resolve(__dirname, '../static')))
   .listen(8080, () => console.log('Server running on 8080...'));
