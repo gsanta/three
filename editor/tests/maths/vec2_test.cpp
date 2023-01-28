ed956ac7142efcb790672f9b85c2e54a79087d93
Gergely Santa <santagergely90@gmail.com>
2023-01-28 17:27:04 +0100

    Fix deploy

--- editor/tests/maths/vec2_test.cpp
+++ editor/tests/maths/vec2_test.cpp
@@ -1,4 +1,4 @@
-//#include "./vec2_test.h"
+
 #include <catch2/catch_test_macros.hpp>
 #include "../src/maths/vec2.h"
 
@@ -9,4 +9,49 @@ TEST_CASE("Adds 2 vectors", "[vec2::operator+]")
 	Vec2 result = Vec2(2, 2) + Vec2(3, 1);
 	REQUIRE(result.x == 5);
 	REQUIRE(result.y == 3);
+}
+
+TEST_CASE("Subtracts 2 vectors", "[vec2::operator-]")
+{
+	Vec2 result = Vec2(2, 2) - Vec2(3, 1);
+	REQUIRE(result.x == -1);
+	REQUIRE(result.y == 1);
+}
+
+TEST_CASE("Multiplies 2 vectors", "[vec2::operator*]")
+{
+	Vec2 result = Vec2(2, 2) * Vec2(3, 1);
+	REQUIRE(result.x == 6);
+	REQUIRE(result.y == 2);
+}
+
+TEST_CASE("Divides 2 vectors", "[vec2::operator/]")
+{
+	Vec2 result = Vec2(6, 2) / Vec2(3, 2);
+	REQUIRE(result.x == 2);
+	REQUIRE(result.y == 1);
+}
+
+TEST_CASE("Adds right vector to left vector", "[vec2::operator+=]")
+{
+	Vec2 vec(2, 1);
+	vec += Vec2(3, 5);
+	REQUIRE(vec.x == 5);
+	REQUIRE(vec.y == 6);
+}
+
+TEST_CASE("Subtracts right vector from left vector", "[vec2::operator-=]")
+{
+	Vec2 vec(3, 5);
+	vec -= Vec2(2, 1);
+	REQUIRE(vec.x == 1);
+	REQUIRE(vec.y == 4);
+}
+
+TEST_CASE("Multiplies right vector to left vector", "[vec2::operator*=]")
+{
+	Vec2 vec(3, 5);
+	vec *= Vec2(2, 3);
+	REQUIRE(vec.x == 6);
+	REQUIRE(vec.y == 15);
 }
