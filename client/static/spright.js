351dece527965a55439ba6a120212256e1fbe51f
CircleCI Job <santagergely90@gmail.com>
2023-01-31 21:34:01 +0000

    Automatic commit from CircleCI [skip ci]

--- client/static/spright.js
+++ client/static/spright.js
@@ -9489,7 +9489,7 @@ var dynCall_iiiiij = Module["dynCall_iiiiij"] = createExportWrapper("dynCall_iii
 var dynCall_iiiiijj = Module["dynCall_iiiiijj"] = createExportWrapper("dynCall_iiiiijj");
 /** @type {function(...*):?} */
 var dynCall_iiiiiijj = Module["dynCall_iiiiiijj"] = createExportWrapper("dynCall_iiiiiijj");
-var ___emscripten_embedded_file_data = Module['___emscripten_embedded_file_data'] = 83088;
+var ___emscripten_embedded_file_data = Module['___emscripten_embedded_file_data'] = 83168;
 
 // include: postamble.js
 // === Auto-generated postamble setup entry stuff ===
