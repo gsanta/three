3be28b9736d0ff89f2cf857d7c41831c51eff148
Sánta Gergely <santagergely90@gmail.com>
2023-05-20 17:18:44 +0200

    refactor: paint bucket tool (#31)

--- editor/src/app/editor.cpp
+++ editor/src/app/editor.cpp
@@ -36,7 +36,7 @@ namespace editor
         m_toolHandler->addTool(new EraserTool(3));
         m_toolHandler->addTool(new PanTool(getDocumentStore()));
         m_toolHandler->addTool(new ZoomTool(getDocumentStore()));
-        m_toolHandler->addTool(new PaintBucketTool(m_Services));
+        m_toolHandler->addTool(new PaintBucketTool());
         m_toolHandler->addTool(new SelectTool());
         m_toolHandler->addTool(new ColorPickerTool(m_toolHandler, m_EventEmitter.get()));
         m_toolHandler->addTool(new NewDrawingTool(getDocumentStore(), m_DocumentFactory));
