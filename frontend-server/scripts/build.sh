e56c9dec7b204145872e469383cb891e3da3912a
Sánta Gergely <santagergely90@gmail.com>
2023-07-23 17:58:01 +0200

    clean: remove unused files and packages (#53)

--- frontend-server/scripts/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -rf dist
-mkdir -p dist/website
-cp -r ../website dist
-cp src/index.js dist
--- frontend-server/src/index.js
+++
@@ -1,8 +0,0 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-var { resolve } = require('path');
-
-
-connect()
-  .use(serveStatic(resolve(__dirname, '../static')))
-  .listen(8080, () => console.log('Server running on 8080...'));
