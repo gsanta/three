890f26aad42574d1f27ef50356fc91f424110bfd
Sánta Gergely <santagergely90@gmail.com>
2023-08-15 22:55:09 +0200

    fix: send authenticity token (#69)

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -3,4 +3,11 @@ class ApplicationController < ActionController::Base
   include ReactRenderer::DSL
   include ErrorHandling
   include ResponseHelper
+
+  after_action :set_csrf_cookie
+
+
+  def set_csrf_cookie
+    cookies["X-CSRF-Token"] = form_authenticity_token
+  end
 end
