d741f71b7f9e0d1919b8ffe6f1baa9b7d0e16fee
Gergely Santa <gergely.santa@bitrise.io>
2024-02-17 20:37:05 +0100

    Basic nextjs transition

---
+++ client/app/features/frame/components/Frames.tsx
@@ -0,0 +1,74 @@
+import FrameButton from '../../../common/components/FrameButton';
+import { Box, Divider } from '@chakra-ui/react';
+import Icon from '../../../common/components/icon/Icon';
+import React from 'react';
+import { Tooltip, Button } from '@chakra-ui/react';
+import { useAppSelector, useAppDispatch } from '@/common/hooks/hooks';
+import { activateFramePlayer, addFrame, deActivateFramePlayer, removeFrame, setActiveFrame } from '../state/frameSlice';
+
+const Frames = () => {
+  const frames = useAppSelector((state) => state.frame.frames);
+  const activeIndex = useAppSelector((state) => state.frame.activeIndex);
+  const isPlaying = useAppSelector((state) => state.frame.isPlaying);
+  const dispatch = useAppDispatch();
+
+  const handleFrameSelect = (index: number) => {
+    dispatch(setActiveFrame(index));
+  };
+
+  const isSingleFrame = frames.length <= 1;
+
+  return (
+    <Box>
+      <Box display="flex" gap="2" justifyContent="end">
+        {isPlaying ? (
+          <Tooltip label="stop frame player">
+            <Button className="iconOnly" onClick={() => dispatch(deActivateFramePlayer())} size="sm">
+              <Icon name="BiStop" />
+            </Button>
+          </Tooltip>
+        ) : (
+          <Tooltip label={isSingleFrame ? "can't play a single frame" : 'start frame player'}>
+            <Button
+              className="iconOnly"
+              onClick={() => dispatch(activateFramePlayer())}
+              size="sm"
+              isDisabled={isSingleFrame}
+            >
+              <Icon name="BiPlay" />
+            </Button>
+          </Tooltip>
+        )}
+
+        <Tooltip label={isSingleFrame ? "can't remove last frame" : 'remove frame'}>
+          <Button
+            className="iconOnly"
+            isDisabled={isSingleFrame}
+            onClick={() => dispatch(removeFrame(activeIndex))}
+            size="sm"
+          >
+            <Icon name="BiTrashAlt" />
+          </Button>
+        </Tooltip>
+        <Tooltip label="new frame">
+          <Button className="iconOnly" onClick={() => dispatch(addFrame())} size="sm">
+            <Icon name="BiPlus" />
+          </Button>
+        </Tooltip>
+      </Box>
+      <Divider marginBlock="2" />
+      {frames.map((frame, index) => (
+        <FrameButton
+          index={index}
+          isActive={index === activeIndex}
+          key={frame.index}
+          onFrameSelect={handleFrameSelect}
+          marginBlockEnd="0.5"
+          marginInlineEnd="0.5"
+        />
+      ))}
+    </Box>
+  );
+};
+
+export default Frames;
---
+++ client/app/features/layer/components/LayerPanel.tsx
@@ -0,0 +1,58 @@
+import Panel from '../../../common/components/Panel';
+import { List, Tooltip, Button } from '@chakra-ui/react';
+import { useBoolean } from 'usehooks-ts';
+import React from 'react';
+import LayerItem from './LayerItem';
+import AddLayerDialog from './AddLayerDialog';
+import LayerDropTarget from './LayerDropTarget';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import Icon from '../../../common/components/icon/Icon';
+import Frames from '../../frame/components/Frames';
+import { useAppDispatch, useAppSelector } from '@/common/hooks/hooks';
+import { setActiveLayer } from '../state/layerSlice';
+
+const LayerPanel = () => {
+  const { value: isAddPanelOpen, setTrue: setOpenAddPanel, setFalse: setCloseAddPanel } = useBoolean(false);
+  const dispatch = useAppDispatch();
+
+  const layers = useAppSelector((state) => state.layer.layers);
+  const activeLayer = useAppSelector((state) => state.layer.activeLayer);
+
+  return (
+    <Panel
+      header={
+        <Panel.Header title="layers & frames">
+          <Tooltip label="new layer">
+            <Button className="iconOnly" onClick={setOpenAddPanel} size="sm">
+              <Icon name="BiPlus" />
+            </Button>
+          </Tooltip>
+        </Panel.Header>
+      }
+      height="100%"
+    >
+      <DndProvider backend={HTML5Backend}>
+        <List flex="1">
+          <LayerDropTarget layerIndex={0} />
+          {layers.map((layer, index) => (
+            <React.Fragment key={layer.name}>
+              <LayerItem
+                isActive={layer.index === activeLayer?.index}
+                key={layer.name}
+                layer={layer}
+                setActiveLayer={() => dispatch(setActiveLayer(layer))}
+                isDeleteDisabled={layers.length === 1}
+              />
+              <LayerDropTarget key={index + 1} layerIndex={index + 1} />
+            </React.Fragment>
+          ))}
+        </List>
+      </DndProvider>
+      <Frames />
+      <AddLayerDialog isOpen={isAddPanelOpen} onClose={setCloseAddPanel} />
+    </Panel>
+  );
+};
+
+export default LayerPanel;
