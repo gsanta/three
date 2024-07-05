6fa09be6b7a12d2a2e3c3c766518a55ab54143fa
Gergely Santa <gergely.santa@bitrise.io>
2024-07-05 21:52:46 +0200

    Remove unused file

--- src/client/editor/use_cases/wiring/BuildingHelper.ts
+++
@@ -1,9 +0,0 @@
-import Block from '../../types/Block';
-
-class BuildingHelper {
-  getWallInputs(wallBlock: Block) {
-    // const  wallBlock.slotTarget?.slotName
-  }
-}
-
-export default BuildingHelper;
