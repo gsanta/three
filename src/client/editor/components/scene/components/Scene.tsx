860734e36ba1c1eb13113941f38dbf4e7d20e653
gsanta <santagergely90@gmail.com>
2024-08-01 00:48:45 +0200

    Fix

--- src/client/editor/components/scene/components/Scene.tsx
+++ src/client/editor/components/scene/components/Scene.tsx
@@ -1,5 +1,5 @@
 import { Environment, OrbitControls, PerspectiveCamera } from '@react-three/drei';
-import { useCallback, useEffect, useMemo, useRef } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 import useEditorContext from '@/app/editor/EditorContext';
 import { ThreeEvent, useThree } from '@react-three/fiber';
 import { useAppSelector } from '@/client/common/hooks/hooks';
